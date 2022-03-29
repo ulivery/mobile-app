d71d597d2e254987bfb2bc5ae5fb8a990ea610df
Alain Lardinois <alain@ip-ict.nl>
2022-03-29 15:04:04 +0200

    Add firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ulivery_mobile_app/pages/Onboarding/pages/onboarding_screen.dart';
 
-
+import 'firebase_options.dart';
 import 'util/theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const UliveryApp());
 }
 
@@ -13,6 +19,14 @@ class UliveryApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        //TODO: Redirect user to login
+      } else {
+        //TODO: Redirect user to home
+      }
+    });
+
     return MaterialApp(
       title: 'Ulivery',
       theme: defaultTheme,
