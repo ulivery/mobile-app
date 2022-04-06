54a8a4e3ae31082d49822139190cb8fe8aae0e61
FluroShift <dwayne-007@hotmail.com>
2022-04-06 15:50:28 +0200

    Contained payment screen in bottom modal

--- lib/pages/wallet/top_up.dart
+++ lib/pages/wallet/top_up.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ulivery_mobile_app/pages/wallet/payment_screen_modal.dart';
 
 class TopUp extends StatelessWidget {
   const TopUp({Key? key}) : super(key: key);
@@ -46,7 +47,18 @@ class TopUpBody extends StatelessWidget {
                           child: Padding(
                             padding: EdgeInsets.all(10.0),
                             child: ElevatedButton(
-                                onPressed: (){},
+                                onPressed: (){
+                                  showModalBottomSheet(
+                                    isScrollControlled: true,
+                                      context: context,
+                                      builder: (context){
+                                        return FractionallySizedBox(
+                                          heightFactor: 0.9,
+                                          child: PaymentScreenModal(),
+                                        );
+                                      },
+                                  );
+                                },
                                 child: const Text(
                                   "€ 5",
                                   style: TextStyle(fontSize: 20),
@@ -59,7 +71,16 @@ class TopUpBody extends StatelessWidget {
                           child: Padding(
                             padding: EdgeInsets.all(10.0),
                             child: ElevatedButton(
-                              onPressed: (){},
+                              onPressed: (){showModalBottomSheet(
+                                isScrollControlled: true,
+                                context: context,
+                                builder: (context){
+                                  return FractionallySizedBox(
+                                    heightFactor: 0.9,
+                                    child: PaymentScreenModal(),
+                                  );
+                                },
+                              );},
                               child: const Text(
                                 "€ 10",
                                 style: TextStyle(fontSize: 20),
@@ -72,7 +93,16 @@ class TopUpBody extends StatelessWidget {
                           child: Padding(
                             padding: EdgeInsets.all(10.0),
                             child: ElevatedButton(
-                              onPressed: (){},
+                              onPressed: (){showModalBottomSheet(
+                                isScrollControlled: true,
+                                context: context,
+                                builder: (context){
+                                  return FractionallySizedBox(
+                                    heightFactor: 0.9,
+                                    child: PaymentScreenModal(),
+                                  );
+                                },
+                              );},
                               child: const Text(
                                 "€ 15",
                                 style: TextStyle(fontSize: 20),
@@ -89,7 +119,16 @@ class TopUpBody extends StatelessWidget {
                           child: Padding(
                             padding: EdgeInsets.all(10.0),
                             child: ElevatedButton(
-                              onPressed: (){},
+                              onPressed: (){showModalBottomSheet(
+                                isScrollControlled: true,
+                                context: context,
+                                builder: (context){
+                                  return FractionallySizedBox(
+                                    heightFactor: 0.9,
+                                    child: PaymentScreenModal(),
+                                  );
+                                },
+                              );},
                               child: const Text(
                                 "€ 20",
                                 style: TextStyle(fontSize: 20),
@@ -102,7 +141,16 @@ class TopUpBody extends StatelessWidget {
                           child: Padding(
                             padding: EdgeInsets.all(10.0),
                             child: ElevatedButton(
-                              onPressed: (){},
+                              onPressed: (){showModalBottomSheet(
+                                isScrollControlled: true,
+                                context: context,
+                                builder: (context){
+                                  return FractionallySizedBox(
+                                    heightFactor: 0.9,
+                                    child: PaymentScreenModal(),
+                                  );
+                                },
+                              );},
                               child: const Text(
                                 "€ 25",
                                 style: TextStyle(fontSize: 20),
@@ -115,7 +163,16 @@ class TopUpBody extends StatelessWidget {
                           child: Padding(
                             padding: EdgeInsets.all(10.0),
                             child: ElevatedButton(
-                              onPressed: (){},
+                              onPressed: (){showModalBottomSheet(
+                                isScrollControlled: true,
+                                context: context,
+                                builder: (context){
+                                  return FractionallySizedBox(
+                                    heightFactor: 0.9,
+                                    child: PaymentScreenModal(),
+                                  );
+                                },
+                              );},
                               child: const Text(
                                 "€ 30",
                                 style: TextStyle(fontSize: 20),
